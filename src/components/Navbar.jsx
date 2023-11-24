a1301507d26f9b2389cfa317ae97cbfbc9dfcf56
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-11-24 18:43:41 +0100

    Created sidebar component for the homepage. Added also sidebar component with style.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <span className='logo'>Saynax</span>
+      <div className='user'>
+      <img src="" alt="" />
+      <span>Johnny Depp</span>
+      <button>Logot</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
