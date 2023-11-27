273a587588bc71daa4873cfb91285e2e93e3b9a0
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-11-28 00:05:46 +0100

    Added function to create chat between two users. Validation of two IDs to avoid duplication.

--- src/components/Serach.jsx
+++ src/components/Serach.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import React, { useContext } from "react";
+import { collection, query, where, getDocs, getDoc, setDoc, updateDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase";
 import { useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -16,18 +19,62 @@ const Search = () => {
 
     try {
       const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      setUser(doc.data())
-    });
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
     } catch (error) {
-      setErr(true)
+      setErr(true);
     }
   };
 
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   };
+
+  const handleSelect = async () => {
+    //check if the group exists, if not, create
+    //This is done to avoid duplication of chats
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+        try {
+          const res = await getDoc(doc(db, "chats", combinedId));
+          if(!res.exists()){
+            //create chat in chat collection
+            await setDoc(doc(db, "chats", combinedId), {messages: [] });
+
+            //user chat
+            await updateDoc(doc(db, "userChats", currentUser.uid), {
+              [combinedId+".userInfo"]: {
+                uid:user.uid,
+                displayName: user.displayName,
+                photoURL: user.photoURL 
+              },
+              [combinedId+"date"]: serverTimestamp()
+            });
+
+            await updateDoc(doc(db, "userChats", user.uid), {
+              [combinedId+".userInfo"]: {
+                uid:currentUser.uid,
+                displayName: currentUser.displayName,
+                photoURL: currentUser.photoURL 
+              },
+              [combinedId+"date"]: serverTimestamp()
+            });
+
+          }
+        } catch (err) {
+          
+        }
+
+        setUser(null)
+        setUsername("")
+  };
+
   
+
   return (
     <div className="search">
       <div className="searchForm">
@@ -36,18 +83,18 @@ const Search = () => {
           placeholder="Search for user"
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
       </div>
       {err && <span>User not found</span>}
-      {user && <div className="userChat">
-        <img
-          src= {user.photoURL}
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>{user.displayName}</span>
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
